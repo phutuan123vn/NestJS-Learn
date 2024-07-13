2e0fc0520026bb11a86b565d598cb40cb7d0a555
phutuan123vn <phutuan123vn@gmail.com>
2024-07-13 17:08:01 +0700

    base pj

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CreateBlogDTO } from './dto';
 import { Blog, User } from '@prisma/client';
+import { keys as getKeys } from 'ts-transformer-keys';
 
 @Injectable()
 export class BlogService {
@@ -21,22 +22,18 @@ export class BlogService {
   }
 
   async getBlogs(
-    blogSelect: { [Key in keyof Blog]: boolean } = Object.fromEntries(
-      (Object.keys({} as Blog)).map((key) => [key, true]),
-    ) as { [K in keyof Blog]: boolean },
-    authorSelect?: { [Key in keyof User]?: boolean },
+    blogSelect: { [Key in keyof Blog]?: boolean } | Array<keyof Blog> = Object.fromEntries(getKeys<Blog>().map((key) => [key, true])),
+    authorSelect: { [Key in keyof User]?: boolean } | Array<keyof User> = ['id', 'email'],
   ) {
-    console.log(blogSelect);
+    const blog = Array.isArray(blogSelect) ? Object.fromEntries(blogSelect.map((key) => [key, true])) : blogSelect;
+    const author = Array.isArray(authorSelect) ? Object.fromEntries(authorSelect.map((key) => [key, true])) : authorSelect;
     return this.prisma.blog.findMany({
       // select: {},
       // select: blogSelect,
       select: {
-        ...blogSelect,
+        ...blog,
         author: {
-          select: {
-            id: true,
-            email: true,
-          },
+          select: author,
         },
       },
     });
