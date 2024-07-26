7f653d07768b5ca858ac2ee18ea76511b342becc
phutuan123vn <phutuan123vn@gmail.com>
2024-07-26 17:43:01 +0700

    done the project

--- src/Events/events.gateway.ts
+++ src/Events/events.gateway.ts
@@ -14,37 +14,43 @@ import { Server, Socket } from 'socket.io';
 import { AuthGuard } from './websocket.guard';
 import { EventsService } from './events.service';
 import { parse } from 'cookie';
+import { User } from '@/types';
+
+type SocketWithAuth = Socket & { auth: User };
 
 @WebSocketGateway({
   cors: {
     origin: '*',
     credentials: true,
   },
+  // cookie: true,
+  // middlware: [AuthGuard],
 })
 // @UseGuards(JwtAuthGuard)
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(
-    private readonly eventsService: EventsService,
-  ){}
+  constructor(private readonly eventsService: EventsService) {}
 
   async handleConnection(client) {
     const cookies = parse(client.handshake.headers.cookie);
-    const payload = await this.eventsService.validateUser(cookies['refreshtoken']);
+    const payload = await this.eventsService.validateUser(
+      cookies['refreshtoken'],
+    );
     if (!payload) {
       client.disconnect();
     }
     client.auth = payload;
-    console.log(client)
+    console.log(client);
+    await this.joinRoom(client, +client.handshake.query.roomID);
   }
-  
+
   // @UseGuards(AuthGuard)
   @SubscribeMessage('events')
   findAll(
     @MessageBody() data: any,
-    @ConnectedSocket() client: Socket,
+    @ConnectedSocket() client: SocketWithAuth,
   ): Observable<WsResponse<number>> {
     console.log(client);
     console.log(data);
@@ -61,8 +67,20 @@ export class EventsGateway {
   @SubscribeMessage('message')
   async handleMessage(
     @MessageBody() data: string,
-    @ConnectedSocket() client: Socket,
+    @ConnectedSocket() client: SocketWithAuth,
   ): Promise<void> {
+    const roomID = +client.handshake.query.roomID;
+    const user = client.auth;
+    const message = await this.eventsService.sendMessage(roomID, data, user);
+    client.to("room_" + roomID).emit('message', message);
     // const message = await this.eventsService.sendMessage(data, client.handshake.query.token);
+  }
+
+  async joinRoom(client: SocketWithAuth, roomID: number) {
+    client.join("room_" + roomID);
+  }
+
+  async leaveRoom(client: SocketWithAuth, roomID: number) {
+    client.leave("room_" + roomID);
   }
 }
